ad9192ddced2aade8db3224288b585b70f06188c
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-03-12 20:46:58 +0800

    Added BaritoneWrapper.java

---
+++ src/main/java/cn/crtlprototypestudios/controlui_refactored/client/BaritoneWrapper.java
@@ -0,0 +1,29 @@
+package cn.crtlprototypestudios.controlui_refactored.client;
+
+import baritone.api.BaritoneAPI;
+import baritone.api.IBaritone;
+import baritone.api.process.IBaritoneProcess;
+import baritone.api.process.PathingCommand;
+
+public class BaritoneWrapper {
+    private static IBaritone instance;
+
+    public static IBaritone getInstance() {
+        if(instance == null){
+            instance = BaritoneAPI.getProvider().getPrimaryBaritone();
+        }
+        return instance;
+    }
+
+    public static void stopAllActions(){
+        getInstance().getCommandManager().execute("stop");
+    }
+
+    public static void pauseAllActions(){
+        getInstance().getCommandManager().execute("pause");
+    }
+
+    public static void resumeAllActions(){
+        getInstance().getCommandManager().execute("resume");
+    }
+}
